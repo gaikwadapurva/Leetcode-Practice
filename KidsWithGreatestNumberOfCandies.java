8264e05262a536435fb7d5dc402be6e7e0f20529
apurva.gai <apurva.gai>
2021-06-29 19:33:27 +0530

    Programs done on 29-06-2021

---
+++ KidsWithGreatestNumberOfCandies.java
@@ -0,0 +1,20 @@
+class KidsWithGreatestNumberOfCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = candies[0];
+        for (int i = 1; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            if ((candies[i] + extraCandies) >= max) {
+                result.add(true);
+            }
+            else {
+                result.add(false);
+            }
+        }
+        return (List)result;
+    }
+}
