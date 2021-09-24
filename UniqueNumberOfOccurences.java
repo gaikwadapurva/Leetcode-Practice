d9460601ff10b85dfbc20e517844f27201fba081
Apurva Gaikwad <gaikwadapurva65@gmail.com>
2021-09-24 15:05:35 +0530

    Added code to find if the frequencies of each element in array are unique

---
+++ UniqueNumberOfOccurences.java
@@ -0,0 +1,43 @@
+/*
+
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+
+*/
+
+class UniqueNumberOfOccurences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.containsKey(arr[i]) == true? map.get(arr[i]) + 1 : 1);
+        }
+        Set<Integer> set = new HashSet<Integer>(map.values());
+        if (set.size() == map.size()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
